647937ac9dbc5c5dcdbe4a627c08dbe8b8d77405
Penyok8 <kooostya1@gmail.com>
2019-11-16 22:06:05 +0200

    Upd character prefab and add loot

--- Gamejam/Assets/Scripts/Inventory/Inventory.cs
+++ Gamejam/Assets/Scripts/Inventory/Inventory.cs
@@ -7,4 +7,12 @@ public class Inventory : MonoBehaviour
     public int capasity;
     public List<ArtifactBase> artifacts;
     //public List<GameObject> UIobjs;
+
+    public void AddArtefact(ArtifactBase art)
+    {
+        if (artifacts.Count < capasity)
+        {
+            artifacts.Add(art);
+        }
+    }
 }
