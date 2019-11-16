723ecc9ba22d2ef9313f09cf4ddd72b63d4df8ad
Penyok8 <kooostya1@gmail.com>
2019-11-16 23:10:58 +0200

    Add models and bow

--- Gamejam/Assets/Scripts/Artefacts/Weapons/AxeScriptableObj.cs
+++ Gamejam/Assets/Scripts/Artefacts/Weapons/AxeScriptableObj.cs
@@ -15,9 +15,11 @@ public class AxeScriptableObj : WeaponScriptableObject
 
     public override void Action()
     {
-        base.Action();
         foreach (var target in targets)
         {
+            int dmg = Random.Range(MinDamage,MaxDamage);
+            Debug.Log($"Hit {target.name} for {dmg} dmg.");
+            target.HealthController.DoDamage(dmg);
             if (data != null)
             {
                 Debug.Log($"Add bleed to {target.name}");
