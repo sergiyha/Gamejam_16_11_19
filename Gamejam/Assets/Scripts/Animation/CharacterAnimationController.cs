0ef9271cbffe8ba9acba2b23c816314f7d4e7bb3
PinkPanter <sugarfree1408@gmail.com>
2019-11-16 17:08:15 +0200

    Fix velocity

--- Gamejam/Assets/Scripts/Animation/CharacterAnimationController.cs
+++ Gamejam/Assets/Scripts/Animation/CharacterAnimationController.cs
@@ -15,13 +15,19 @@ public class CharacterAnimationController : MonoBehaviour
 
     public Animator Animator => animator;
 
+    [SerializeField]
     protected Vector3 smootherVelocity;
+    protected Vector3 lastPos;
 
 
     protected virtual void Update()
     {
-        smootherVelocity = Vector3.Lerp(smootherVelocity, transform.InverseTransformDirection(agent.velocity), Time.smoothDeltaTime * 15f);
+        var vel = (transform.position - lastPos) / Time.deltaTime;
+        smootherVelocity = Vector3.Lerp(smootherVelocity, transform.InverseTransformDirection(vel), Time.smoothDeltaTime * 15f);
+        smootherVelocity = transform.InverseTransformDirection(vel);//Vector3.Lerp(smootherVelocity, transform.InverseTransformDirection(vel), Time.smoothDeltaTime * 15f);
 
-        animator.SetFloat(SpeedPar, 0.5f + Mathf.Clamp01(smootherVelocity.x / agent.speed));
+        animator.SetFloat(SpeedPar, 0.5f + Mathf.Clamp01(smootherVelocity.magnitude / agent.speed));
+
+        lastPos = transform.position;
     }
 }
