84dda237dc7b6600a1ec5e242ce346bac03f76d5
Sergey Galanov <dukalis.rak@gmail.com>
2019-11-16 19:56:43 +0200

    - base player movement

---
+++ Gamejam/Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class CameraController : MonoBehaviour
+{
+	private PlayerController _playerController;
+
+	[SerializeField] private Camera _camera;
+
+	[SerializeField]
+	private float CameraInvesremultiplier;
+
+	[SerializeField] private Vector3 _cameraDif;
+	// Start is called before the first frame update
+	void Start()
+	{
+		_playerController = GetComponent<PlayerController>();
+		_cameraDif = _camera.transform.position - _playerController.transform.position;
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		var cameraDirection = _playerController.GetDirection() * -1;
+		Debug.DrawRay(_playerController.transform.position, cameraDirection, Color.blue);
+
+		var playerPosition = _playerController.transform.position;
+		_camera.transform.position = playerPosition + _cameraDif - cameraDirection.normalized * CameraInvesremultiplier;
+	}
+
+	private Vector3 _currentCameraInversion = Vector3.zero;
+	private float _lerp = 0;
+
+	//private void LerpCameraInversion(Vector3 futureInversion)
+	//{
+	//	_currentCameraInversion = Vector3.Lerp(_currentCameraInversion, futureInversion, _lerp);
+		
+	//}
+}
---
+++ Gamejam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,142 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField]
+	private Transform[] _startPositions;
+
+	[SerializeField]
+	private Camera _camera;
+
+	[SerializeField]
+	private Transform _mainPlayerObject;
+
+	[SerializeField]
+	private Transform _basePositionsContainer;
+
+	[SerializeField]
+	private Transform _directionObject;
+
+	[SerializeField]
+	private string DirectionDetectorName = "DirectionDetector";
+
+
+	[SerializeField]
+	private NavMeshAgent[] _rangeAgents;
+
+	[SerializeField]
+	private NavMeshAgent[] _meleAgents;
+
+	[SerializeField]
+	private Transform[] _meleAgentsPositions;
+
+	[SerializeField]
+	private Transform[] _rangedAgentsPositions;
+
+	[SerializeField] private float _speed;
+
+	[SerializeField] private float MeleRange;
+	[SerializeField] private float RangeRange;
+
+	// Start is called before the first frame update
+
+	private void Update()
+	{
+		FindDirection();
+		MoveAgents();
+		MoveRanged();
+	}
+
+	public void AddNewUnit()
+	{
+
+	}
+
+	public void RemoveUnit()
+	{
+	}
+
+	public Vector3 GetDirection()
+	{
+		return _aimingDirection;
+	}
+
+	private Vector3 _aimingDirection;
+
+	private Vector3 MousePositionHit;
+	private void FindDirection()
+	{
+		var mousePosition = Input.mousePosition;
+
+		RaycastHit[] hits;
+		var rayMouse = _camera.ScreenPointToRay(mousePosition);
+		hits = Physics.RaycastAll(rayMouse.origin, rayMouse.direction, 100000);
+		Debug.DrawRay(rayMouse.origin, rayMouse.direction * 10000, Color.black);
+		MoveUnits();
+		foreach (var raycastHit in hits)
+		{
+			if (raycastHit.transform.name == DirectionDetectorName)
+			{
+				MousePositionHit = raycastHit.point;
+				var mouseRayHit = raycastHit.point;
+				Debug.DrawLine(MousePositionHit, transform.position);
+ 
+				//Debug.DrawRay(rayMouse.origin,mouseRayHit);
+
+				var mouse0Y = new Vector3(mouseRayHit.x, 0, mouseRayHit.z);
+				var trans0Y = new Vector3(transform.position.x, 0, transform.position.z);
+				_aimingDirection = mouse0Y - trans0Y;
+				_directionObject.forward = _aimingDirection;
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Debug.Break();
+		}
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.DrawSphere(MousePositionHit, 1);
+	}
+
+	private void MoveAgents()
+	{
+		MoveMele();
+	}
+
+	private void MoveMele()
+	{
+		for (var i = 0; i < _meleAgents.Length; i++)
+		{
+			_meleAgents[i].destination = _meleAgentsPositions[i].transform.position;
+		}
+	}
+
+	private void MoveRanged()
+	{
+		for (var i = 0; i < _rangeAgents.Length; i++)
+		{
+			_rangeAgents[i].destination = _rangedAgentsPositions[i].transform.position;
+		}
+	}
+
+	private void MoveUnits()
+	{
+		var y = Input.GetAxis("Vertical") * _speed;
+		var x = Input.GetAxis("Horizontal") * _speed;
+		y *= Time.deltaTime;
+		x *= Time.deltaTime;
+
+		this.transform.position += new Vector3(x, 0, y);
+	}
+
+
+
+
+}
+
+
