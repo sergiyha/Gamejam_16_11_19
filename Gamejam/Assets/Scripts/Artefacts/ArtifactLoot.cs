3ec437e0aa5a547cc78da1a630c04aa963addcc6
Penyok8 <kooostya1@gmail.com>
2019-11-17 13:33:10 +0200

    Finalizing loot.

--- Gamejam/Assets/Scripts/Artefacts/ArtifactLoot.cs
+++ Gamejam/Assets/Scripts/Artefacts/ArtifactLoot.cs
@@ -6,6 +6,31 @@ using UnityEngine;
 public class ArtifactLoot : MonoBehaviour
 {
     public ArtifactBase ArtifactScriptable;
+    private ParticleSystem[] ps;
+    public float pickupTime = 2f;
+    private Light[] spotlight;
+
+    private void Awake()
+    {
+        ps = GetComponentsInChildren<ParticleSystem>();
+        spotlight = GetComponentsInChildren<Light>();
+    }
+
+    private IEnumerator ChangeIntensity()
+    {
+        var koef = 0f;
+        var originalInt = spotlight[0].intensity;
+        while (koef<pickupTime)
+        {
+            foreach (var light in spotlight)
+            {
+                light.intensity = originalInt * ((pickupTime - koef) / pickupTime);
+            }
+            yield return new WaitForEndOfFrame();
+            koef += Time.deltaTime;
+        }
+        
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,8 +40,17 @@ public class ArtifactLoot : MonoBehaviour
             Character character = other.GetComponent<Character>();
             if (character.CharacterType == Character.CharType.Player)
             {
-                SquadInventory.Instance.AddArtefact(ArtifactScriptable);
+                Debug.Log("pickup loot");
+                if(SquadInventory.Instance!=null)
+                    SquadInventory.Instance.AddArtefact(ArtifactScriptable);
+                var main = ps[0].main;
+                main.loop = false;
+                StartCoroutine(ChangeIntensity());
+                main.simulationSpeed *= 2f;
+                var main2 = ps[1].main;
+                main2.gravityModifier = new ParticleSystem.MinMaxCurve(-10f);
             }
+            Destroy(gameObject,pickupTime);
         }
     }
 }
