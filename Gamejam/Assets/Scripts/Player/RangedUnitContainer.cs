f85e5b272212045dce6ffa841f2f99688fa74df4
Sergey Galanov <dukalis.rak@gmail.com>
2019-11-17 21:07:00 +0200

    Merge branch 'Aiming+Pink' into Pink
    # Conflicts:
    #	Gamejam/Assets/Data/Goblin/died.meta
    #	Gamejam/Assets/Data/Goblin/idle.meta
    #	Gamejam/Assets/Data/Goblin/walk.meta
    #	Gamejam/Assets/Fonts.meta
    #	Gamejam/Assets/PlayerModels.unity.meta
    #	Gamejam/Assets/PlayerMovementScene.unity
    #	Gamejam/Assets/Plugins/Sirenix/Demos.meta
    #	Gamejam/Assets/Prefabs/HasteTrail.prefab.meta
    #	Gamejam/Assets/Scripts/Player/RangedUnitContainer.cs
    #	Gamejam/Assets/q.unity.meta

--- Gamejam/Assets/Scripts/Player/RangedUnitContainer.cs
+++ Gamejam/Assets/Scripts/Player/RangedUnitContainer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -10,18 +11,18 @@ public class RangedUnitContainer : UnitContainer
 
 	public override void Move(Vector3 forward)
 	{
-        for (var i = 0; i < _agents.Length; i++)
-        {
-            _agents[i].destination = _localTargets[i].position;
-            _agents[i].updateRotation = false;
-            _agents[i].transform.forward = forward;
-        }
-    }
+		for (var i = 0; i < _agents.Length; i++)
+		{
+			_agents[i].destination = _localTargets[i].position;
+		}
+	}
 
 	public void TryAttack()
 	{
 		if (_enemies == null || !_enemies.Any())
 		{
+			if (_characters == null || !_characters.Any()) return;
+
 			for (int i = 0; i < _characters.Count; i++)
 			{
 				_characters[i].AimingModule.Disable();
@@ -30,15 +31,23 @@ public class RangedUnitContainer : UnitContainer
 		}
 
 		var enemy = _enemies.First();
-		
+
+		if (_characters == null || !_characters.Any()) return;
 
 		for (int i = 0; i < _characters.Count; i++)
 		{
-			_characters[i].AimingModule.Aim(enemy.Character.transform);
-        }
-    }
+			var bow = _characters[i].WeaponController.Weapon as BowScriptableObj;
+		   _characters[i].AimingModule.Aim(enemy.Character.transform, bow, _characters[i]);
+		}
+	}
 
+	
+	
 
+	private void Fire(int ind)
+	{
+
+	}
 
 
 
