6c05d58e91b333ec34cc87c5568259ef0caca967
Penyok8 <kooostya1@gmail.com>
2019-11-17 18:48:50 +0200

    Upd add sword

---
+++ Gamejam/Assets/Scripts/Characters/StatusEffects/SlowEffectData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SlowEffect", menuName = "Effects/Slow")]
+public class SlowEffectData : BaseEffectData
+{
+    public float tickTime;
+    public float totalTime;
+    public float speedMultiplier = 0.7f;
+    
+}
+
